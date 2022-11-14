652a7fb7e60cb5b6ee088b0224efb1ce914fe58d
Le Van Luan <lvluan0902dev@gmail.com>
2022-11-14 21:16:48 +0700

    PJ, AboutUs - Refactor, add some content

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,11 +10,11 @@ const routes: Routes = [
     component: HomeComponent
   },
   {
-    path: 'shop',
+    path: 'cua-hang',
     component: ShopComponent
   },
   {
-    path: 'about-us',
+    path: 'gioi-thieu',
     component: AboutUsComponent
   }
 ];
