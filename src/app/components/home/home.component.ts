e9afc2bff087186244723b85cda266e3a4f7c4f8
Le Van Luan <lvluan0902dev@gmail.com>
2022-11-18 20:51:27 +0700

    Home - Completed slick slider, testimonial, beauty image

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { AfterViewChecked, AfterViewInit, Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { BeautyImage } from 'src/app/models/beauty-image/beauty-image.model';
 import { Slider } from 'src/app/models/slider/slider.model';
@@ -14,17 +14,40 @@ declare var themeInit: any;
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit, AfterViewInit {
+export class HomeComponent implements OnInit, AfterViewInit, AfterViewChecked {
   public url = environment.url + '/';
   public sliders: Slider[] = [];
   public testimonials: Testimonial[] = [];
   public beautyImages: BeautyImage[] = [];
+  private s_slider_slicked: boolean = false;
+  private s_testimonial_slicked: boolean = false;
+  private s_beautyImage_slicked: boolean = false;
 
   constructor(
     private title: Title,
     private homeService: HomeService
   ) {
     this.title.setTitle('Trang chủ');
+  }
+
+  ngAfterViewChecked(): void {
+    if (!this.s_slider_slicked && this.sliders.length) {
+      $(".s-slider").slick("unslick");
+      $(".s-slider").slick({"slidesToShow": 1,"infinite":true,"autoplay":true,"dots":false,"arrows":false,"fade":true,"cssEase":"ease-in-out","speed":600});
+      this.s_slider_slicked = true;
+    }
+    
+    if (!this.s_testimonial_slicked && this.testimonials.length) {
+      $(".s-testimonial").slick("unslick");
+      $(".s-testimonial").slick({"slidesToShow": 1,"infinite":true,"autoplay":true,"dots":false,"arrows":false,"fade":false,"cssEase":"ease-in-out","speed":600});
+      this.s_testimonial_slicked = true;
+    }
+
+    if (!this.s_beautyImage_slicked && this.beautyImages.length) {
+      $(".s-beauty-image").slick("unslick");
+      $(".s-beauty-image").slick({"slidesToShow": 5,"infinite":false,"autoplay":true,"dots":false,"arrows":false,"responsive":[{"breakpoint": 1366,"settings": {"slidesToShow":5}},{"breakpoint": 992,"settings": {"slidesToShow":4}},{"breakpoint": 768,"settings": {"slidesToShow": 3}},{"breakpoint": 576,"settings": {"slidesToShow": 2}}]});
+      this.s_beautyImage_slicked = true;
+    }
   }
 
   ngAfterViewInit(): void {
@@ -41,6 +64,7 @@ export class HomeComponent implements OnInit, AfterViewInit {
     this.homeService.getAllSlider().subscribe((response) => {
       if (response.success == 1) {
         this.sliders = response.data;
+        this.s_slider_slicked = false;
       }
     });
   }
@@ -49,6 +73,7 @@ export class HomeComponent implements OnInit, AfterViewInit {
     this.homeService.getAllTestimonial().subscribe((response) => {
       if (response.success == 1) {
         this.testimonials = response.data;
+        this.s_testimonial_slicked = false;
       }
     });
   }
@@ -57,6 +82,7 @@ export class HomeComponent implements OnInit, AfterViewInit {
     this.homeService.getAllBeautyImage().subscribe((response) => {
       if (response.success == 1) {
         this.beautyImages = response.data;
+        this.s_beautyImage_slicked = false;
       }
     });
   }
