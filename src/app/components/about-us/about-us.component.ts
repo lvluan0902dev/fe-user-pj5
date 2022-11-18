d161d729e8b1e6830260fbb697a10c913d0adaed
Le Van Luan <lvluan0902dev@gmail.com>
2022-11-18 21:02:08 +0700

    AboutUs - Completed slick staff

--- src/app/components/about-us/about-us.component.ts
+++ src/app/components/about-us/about-us.component.ts
@@ -1,9 +1,10 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { AfterViewChecked, AfterViewInit, Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { Staff } from 'src/app/models/staff/staff.model';
 import { AboutUsService } from 'src/app/services/about-us/about-us.service';
 import { environment } from 'src/environments/environment';
 
+declare var $: any;
 declare var themeInit: any;
 
 @Component({
@@ -11,22 +12,24 @@ declare var themeInit: any;
   templateUrl: './about-us.component.html',
   styleUrls: ['./about-us.component.css']
 })
-export class AboutUsComponent implements OnInit, AfterViewInit {
+export class AboutUsComponent implements OnInit, AfterViewInit, AfterViewChecked {
   public url = environment.url + '/';
   public staffs: Staff[] = [];
-  public slideConfig = {
-    "slidesToShow": 4,
-    "dots": false,
-    "arrows": false,
-    "responsive":
-      [{ "breakpoint": 1200, "settings": { "slidesToShow": 3, "arrows": false, "dots": true } }, { "breakpoint": 768, "settings": { "slidesToShow": 2, "arrows": false, "dots": true } }, { "breakpoint": 576, "settings": { "slidesToShow": 1, "arrows": false, "dots": true } }]
-  }
+  private s_staff_slicked: boolean = false;
 
   constructor(
     private title: Title,
     private aboutUsService: AboutUsService
   ) {
     this.title.setTitle('Giới thiệu');
+  }
+
+  ngAfterViewChecked(): void {
+    if (!this.s_staff_slicked && this.staffs.length) {
+      $(".s-staff").slick("unslick");
+      $(".s-staff").slick({"slidesToShow": 4,"dots":false,"arrows":false,"responsive":[{"breakpoint": 1200,"settings": {"slidesToShow": 3,"arrows":false,"dots":true}},{"breakpoint": 768,"settings": {"slidesToShow": 2,"arrows":false,"dots":true}},{"breakpoint": 576,"settings": {"slidesToShow": 1,"arrows":false,"dots":true}}]});
+      this.s_staff_slicked = true;
+    }
   }
 
   ngAfterViewInit(): void {
@@ -41,6 +44,7 @@ export class AboutUsComponent implements OnInit, AfterViewInit {
     this.aboutUsService.getAllStaff().subscribe((response) => {
       if (response.success == 1) {
         this.staffs = response.data;
+        this.s_staff_slicked = false;
       }
     })
   }
